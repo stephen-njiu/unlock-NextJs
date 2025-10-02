861b2cf1128c25f4b9fe811f6860833ea1b4bdae
njiutheguru <stephennjiu@gmail.com>
2025-10-02 14:09:36 +0300

    added dashboard

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -5,12 +5,12 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 text-blue-600">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
+        <div className="w-32 text-blue-600 md:w-40">
           <AcmeLogo />
         </div>
       </Link>
